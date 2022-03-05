18e2297d7cef50bf230298a427a8d3a0bd00e9eb
romanvijay1@gmail.com <romanvijay1@gmail.com>
2022-03-05 12:23:06 +0530

    path change

--- Datanalaysis/firstdemo.py
+++ Datanalaysis/firstdemo.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 #df = pd.read_csv("D:\\userdetails\\Python_class\\Datanalaysis\\demo.csv",encoding='utf-8')
 
-df = pd.read_csv('D:\\userdetails\\Python_class\\Datanalaysis\\acc3.csv',) #, encoding='ISO-8859-1')
+df = pd.read_csv('D:\\Python_class\\Datanalaysis\\acc3.csv',) #, encoding='ISO-8859-1')
 
 # print(df.head(10))
 
